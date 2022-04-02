bc4c67ae1aeb33e7eef656b2caf85914504ba80e
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-04-02 11:56:02 -0700

    compiles but emac_enc28j60_init(1020): vefiry chip ID failed

---
+++ examples/ESP32-SSH-Server/main/ssh_server.h
@@ -0,0 +1,62 @@
+#pragma once
+
+/* server.h
+ *
+ * Copyright (C) 2014-2021 wolfSSL Inc.
+ *
+ * This file is part of wolfSSH.
+ *
+ * wolfSSH is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wolfSSH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef _WOLFSSH_EXAMPLES_SERVER_H_
+#define _WOLFSSH_EXAMPLES_SERVER_H_
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#define NO_FILESYSTEM
+#define WOLFSSH_TEST_SERVER
+#define WOLFSSH_TEST_THREADING
+
+#undef  WOLFSSL_USER_SETTINGS
+
+#ifdef WOLFSSL_USER_SETTINGS
+#include <wolfssl/wolfcrypt/settings.h>
+#else
+#include <wolfssl/options.h>
+#endif
+
+#include <wolfssl/wolfcrypt/sha256.h>
+#include <wolfssl/wolfcrypt/coding.h>
+#include <wolfssh/ssh.h>
+#include <wolfssh/test.h>
+#include <wolfssl/wolfcrypt/ecc.h>
+
+
+#ifdef NO_FILESYSTEM
+#include <wolfssh/certs_test.h>
+#ifdef WOLFSSH_SCP
+#include <wolfssh/wolfscp.h>
+#endif
+#endif
+
+
+
+THREAD_RETURN WOLFSSH_THREAD server_test();
+
+
+#endif /* _WOLFSSH_EXAMPLES_SERVER_H_ */
