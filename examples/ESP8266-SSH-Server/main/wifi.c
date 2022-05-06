6aa9cf2aca8a1ef30053c7f8ffdba19bffc1f2bf
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-05-05 18:12:21 -0700

    fix esp_event_handler_unregister wifi_STA_event_handler

--- examples/ESP8266-SSH-Server/main/wifi.c
+++ examples/ESP8266-SSH-Server/main/wifi.c
@@ -152,8 +152,8 @@ void wifi_init_sta(void) {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_STA_event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_STA_event_handler));
     vEventGroupDelete(s_wifi_event_group);
 }
 
