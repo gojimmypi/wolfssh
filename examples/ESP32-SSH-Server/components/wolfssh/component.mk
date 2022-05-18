ef51a05f69bdc3bf03ea2d504451b2d2441e7844
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-05-18 08:13:23 -0700

    use wolfssl component wolfcrypt, not a local copy

--- examples/ESP32-SSH-Server/components/wolfssh/component.mk
+++ examples/ESP32-SSH-Server/components/wolfssh/component.mk
@@ -5,13 +5,13 @@
 COMPONENT_ADD_INCLUDEDIRS := . ./include
 COMPONENT_ADD_INCLUDEDIRS += /Users/gojimmypi/Desktop/esp-idf/components/freertos/include/freertos
 
-COMPONENT_SRCDIRS := src wolfcrypt/src
-COMPONENT_SRCDIRS += wolfcrypt/src/port/Espressif
-COMPONENT_SRCDIRS += wolfcrypt/src/port/atmel
+COMPONENT_SRCDIRS := src ../wolfssl/wolfcrypt/src
+COMPONENT_SRCDIRS += ../wolfssl/wolfcrypt/src/port/Espressif
+COMPONENT_SRCDIRS += ../wolfssl/wolfcrypt/src/port/atmel
 
 CFLAGS +=-DWOLFSSL_USER_SETTINGS
 
-COMPONENT_OBJEXCLUDE := wolfcrypt/src/aes_asm.o
-COMPONENT_OBJEXCLUDE += wolfcrypt/src/evp.o
-COMPONENT_OBJEXCLUDE += wolfcrypt/src/misc.o
+COMPONENT_OBJEXCLUDE := ../wolfssl/wolfcrypt/src/aes_asm.o
+COMPONENT_OBJEXCLUDE += ../wolfssl/wolfcrypt/src/evp.o
+COMPONENT_OBJEXCLUDE += ../wolfssl/wolfcrypt/src/misc.o
 COMPONENT_OBJEXCLUDE += src/bio.o
