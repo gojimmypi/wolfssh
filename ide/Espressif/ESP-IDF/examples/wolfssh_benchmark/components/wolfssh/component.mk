7f1831b97510bdabd22393a09b1b93049a759c16
gojimmypi <jim@wolfssl.com>
2023-04-10 12:38:18 +0200

    sample component.mk

--- ide/Espressif/ESP-IDF/examples/wolfssh_benchmark/components/wolfssh/component.mk
+++ ide/Espressif/ESP-IDF/examples/wolfssh_benchmark/components/wolfssh/component.mk
@@ -26,15 +26,8 @@ COMPONENT_ADD_INCLUDEDIRS := . ./include
 COMPONENT_ADD_INCLUDEDIRS += "$ENV{IDF_PATH}/components/freertos/include/freertos"
 # COMPONENT_ADD_INCLUDEDIRS += "$ENV{IDF_PATH}/soc/esp32s3/include/soc"
 
-COMPONENT_SRCDIRS := src wolfcrypt/src
-COMPONENT_SRCDIRS += wolfcrypt/src/port/Espressif
-COMPONENT_SRCDIRS += wolfcrypt/src/port/atmel
-COMPONENT_SRCDIRS += wolfcrypt/benchmark
-COMPONENT_SRCDIRS += wolfcrypt/test
+COMPONENT_SRCDIRS := src
 
 CFLAGS +=-DWOLFSSL_USER_SETTINGS
 
-COMPONENT_OBJEXCLUDE := wolfcrypt/src/aes_asm.o
-COMPONENT_OBJEXCLUDE += wolfcrypt/src/evp.o
-COMPONENT_OBJEXCLUDE += wolfcrypt/src/misc.o
-COMPONENT_OBJEXCLUDE += src/bio.o
+COMPONENT_OBJEXCLUDE := ""
